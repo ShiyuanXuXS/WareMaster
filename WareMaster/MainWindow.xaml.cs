c4e54b80dac8d47d37193ff6882ff459f543d404
JingWeiCandy <2343458@johnabbottcollege.net>
2023-11-07 22:38:44 -0500

    add pagination to mainwindow

--- WareMaster/MainWindow.xaml.cs
+++ WareMaster/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
@@ -47,6 +48,11 @@ namespace WareMaster
     }
     public partial class MainWindow : Window
     {
+        private int currentPage = 1;
+        private int pageSize = 10;
+        private int totalPage = 0;
+        private List<ItemViewModel> allItems = new List<ItemViewModel>() ;
+        private List<ItemViewModel> filterItems = new List<ItemViewModel>();
         public MainWindow()
         {
             InitializeComponent();
@@ -54,8 +60,42 @@ namespace WareMaster
             //InitializeComponent();
             Globals.wareMasterEntities = new WareMasterEntities();
             InitializeLvInit();
+            totalPage = (int)Math.Ceiling((double)allItems.Count / pageSize);
+            for (int i = 0; i < totalPage; i++)
+            {
+                Button newPageButton = new Button()
+                {
+                    Content = i+1,
+                    Width = 15,
+                    Height = 15,
+                    FontSize = 10,
+                    VerticalAlignment = VerticalAlignment.Center,
+                };
+                newPageButton.Click += NewPageButton_Click;
+                StackPaging.Children.Insert(i+2,newPageButton);
+            }
         }
 
+        private void NewPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button clickedButton =(Button)sender;
+            String page = clickedButton.Content.ToString();
+            if (int.TryParse(page, out int currentPage))
+            {
+                DisplayPage(currentPage);
+            }
+            else
+            {
+                MessageBox.Show(this,"Somthing went wrong, will display first page of items.","error",MessageBoxButton.OK,MessageBoxImage.Exclamation);
+                DisplayPage(1);
+            }
+        }
+
+        private void DisplayPage(int page)
+        {
+            var itemsToDisplay = filterItems.Skip((page-1)*pageSize).Take(pageSize).ToList();
+            DgStorage.ItemsSource = itemsToDisplay;
+        }
 
         private void InitializeLvInit()
         {
@@ -64,7 +104,7 @@ namespace WareMaster
                 var query = from item in Globals.wareMasterEntities.Items
                             join settlement in Globals.wareMasterEntities.Settlements on item.id equals settlement.Item_Id into gj
                             from sub in gj.DefaultIfEmpty()
-                            select new
+                            select new ItemViewModel
                             {
                                 ItemId = item.id,
                                 ItemName = item.Itemname,
@@ -77,8 +117,10 @@ namespace WareMaster
                                 SettleDate = sub != null ? sub.Settle_Date : DateTime.Now,
                                 SettlementId = sub != null ? sub.id : -1
                             };
-                DgStorage.ItemsSource = query.ToList();
-                TxblItemCount.Text = query.Count().ToString() + " Items";
+                allItems = query.ToList();
+                filterItems = allItems;
+                DisplayPage(currentPage);
+                TxblItemCount.Text = "Total " + query.Count().ToString() + " Items";
             }
             catch (Exception ex) { MessageBox.Show(ex.Message); }
 
@@ -118,7 +160,7 @@ namespace WareMaster
                 else
                 {
                     this.WindowState = WindowState.Maximized;
-
+                    //GridContent.Height = 920;
                     IsMaximized = true;
                 }
             }
@@ -161,5 +203,58 @@ namespace WareMaster
             }
             catch (Exception ex) { MessageBox.Show(ex.Message); };
         }
+
+        private void BtnPrevPage_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentPage > 1)
+            {
+                currentPage--;
+                DisplayPage(currentPage);
+            }
+        }
+
+        private void BtnNextPage_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentPage < totalPage)
+            {
+                currentPage++;
+                DisplayPage(currentPage);
+            }
+        }
+
+
+
+        private void txtFilter_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtFilter.Text=="")
+            {
+                filterItems = allItems;
+                currentPage = 1;
+                DisplayPage(currentPage);
+            }
+            else
+            {
+                filterItems = new List<ItemViewModel>(from item in allItems
+                              where item.ItemName.Contains(txtFilter.Text.Trim())
+                              select item);
+                currentPage = 1;
+                DisplayPage(currentPage);
+            }
+        }
+
+    }
+
+    public class ItemViewModel
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public string CategoryName { get; set; }
+        public string Unit { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public decimal Total { get; set; }
+        public DateTime SettleDate { get; set; }
+        public int SettlementId { get; set; }
     }
 }
